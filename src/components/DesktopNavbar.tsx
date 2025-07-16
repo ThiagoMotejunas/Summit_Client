a4c913ddc2ae64512188cbb4ee1adfdeb2967f25
VitongoviskDev <v_campos_dev@outlook.com>
2025-07-16 13:47:47 -0300

    page

--- src/components/DesktopNavbar.tsx
+++ src/components/DesktopNavbar.tsx
@@ -1,30 +1,15 @@
 import React, { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { FaBars, FaGraduationCap, FaHome, FaStar } from 'react-icons/fa';
-import { GrUserWorker } from 'react-icons/gr';
+import { Link } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 import { IoPerson } from 'react-icons/io5';
 import { FiLogOut } from 'react-icons/fi';
 
-
 import MenuButtons from '../util/menu';
 import DesktopNavbarButton from './DesktopNavbarButton';
+import { FaBars } from 'react-icons/fa';
 
 const DesktopNavbar: React.FC = () => {
     const { user, logout } = useAuth();
-    const [isOpen, setIsOpen] = useState<boolean>(false);
-    const location = useLocation();
-
-    const toggleMenu = () => setIsOpen((prev) => !prev);
-
-    const closeMenu = () => setIsOpen(false)
-
-    const handleLogoutButtonClick = () => {
-        setIsOpen(false);
-        if (user) {
-            logout();
-        }
-    }
 
     return (
         <header className='fixed top-0 w-full h-18 bg-white shadow-md hidden lg:block'>
@@ -34,14 +19,14 @@ const DesktopNavbar: React.FC = () => {
                     <h1 className='text-blue-400 text-2xl font-bold text-nowrap' style={{ fontFamily: 'Fascinate, cursive' }}>Projeto Summit</h1>
                 </div>
                 {/* MENU */}
-                <div className={`${!isOpen ? 'hidden' : 'realtive'} w-full bg-white py-4 shadow shadow-md shadow-gray-900/20 lg:flex lg:py-0 lg:items-center lg:justify-between lg:w-full lg:h-full lg:shadow-none xl:flex-2/3`}>
+                <div className={`realtive w-full bg-white py-4 shadow shadow-md shadow-gray-900/20 lg:flex lg:py-0 lg:items-center lg:justify-between lg:w-full lg:h-full lg:shadow-none xl:flex-2/3`}>
                     {/* MENU LINKS */}
                     <div className='flex items-center h-full gap-2 xl:w-1/2 xl:justify-center'>
                         {
                             MenuButtons.map((button, index) => (
                                 button.desktop &&
-                                
-                                <DesktopNavbarButton key={index} link={button.link} icon={button.icon} label={button.label}/> 
+
+                                <DesktopNavbarButton key={index} link={button.link} icon={button.icon} label={button.label} />
                             ))
                         }
                     </div>
@@ -49,44 +34,38 @@ const DesktopNavbar: React.FC = () => {
                     <div className='mt-2 lg:mt-0 xl:w-content'>
                         {
                             user ?
-                                (
-                                    <div className='flex items-center justify-end gap-2 w-full'>
-                                        <button className="flex justify-center items-center gap-2 w-full bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700" onClick={closeMenu}>
-                                            <IoPerson />
-                                            <span>Perfil</span>
-                                        </button>
-                                        <button className="flex justify-center items-center gap-2 ring ring-blue-600 text-blue-600 font-medium px-4 py-2 rounded-md hover:bg-blue-700" onClick={handleLogoutButtonClick}>
-                                            <FiLogOut />
-                                            <span>Sair</span>
-                                        </button>
-                                    </div>
-                                ) : (
+                            (
+                                <div className='flex items-center justify-end gap-2 w-full'>
+                                    <Link
+                                        to={'/profile'}
+                                        className="flex items-center gap-2 w-full bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 text-center">
+                                        <IoPerson />
+                                        Perfil
+                                    </Link>
+                                    <button className="flex justify-center items-center gap-2 ring ring-blue-600 text-blue-600 font-medium px-4 py-2 rounded-md hover:bg-blue-700" onClick={logout}>
+                                        <FiLogOut />
+                                        <span>Sair</span>
+                                    </button>
+                                </div>
+                            ) : (
 
-                                    < div className='flex items-center gap-2 w-full'>
-                                        <Link
-                                            to={'/login'}
-                                            className="w-full bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 text-center"
-                                            onClick={closeMenu}>
-                                            Entrar
-                                        </Link>
-                                        <Link
-                                            to={'/register'}
-                                            className="w-full ring ring-blue-600 text-blue-600 font-medium px-4 py-2 rounded-md hover:bg-blue-700 text-center"
-                                            onClick={closeMenu}>
-                                            Cadastrar
-                                        </Link>
-                                    </div>
-                                )
+                                < div className='flex items-center gap-2 w-full'>
+                                    <Link
+                                        to={'/login'}
+                                        className="w-full bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 text-center">
+                                        Entrar
+                                    </Link>
+                                    <Link
+                                        to={'/register'}
+                                        className="w-full ring ring-blue-600 text-blue-600 font-medium px-4 py-2 rounded-md hover:bg-blue-700 text-center">
+                                        Cadastrar
+                                    </Link>
+                                </div>
+                            )
                         }
                     </div>
                 </div>
             </nav>
-            {/* BURGER MENU BUTTON */}
-            <div className='lg:hidden absolute right-4 top-4 text-2xl'>
-                <button onClick={toggleMenu}>
-                    <FaBars />
-                </button>
-            </div>
         </header >
     );
 };
