5944ada65350ed5c5cdc3a8081dbcbac154be2c3
VitongoviskDev <v_campos_dev@outlook.com>
2025-07-16 13:00:13 -0300

    Fix: navbar effetct

--- src/components/DesktopNavbarButton.tsx
+++ src/components/DesktopNavbarButton.tsx
@@ -23,10 +23,10 @@ const DesktopNavbarButton: React.FC<NavbarButtonProps> = ({ link, icon, label, c
             className={`
                 relative flex items-center gap-2 h-full px-4 text-gray-700 text-nowrap text-lg 
                 transition duration-300 ease-in-out
-                after:absolute after:bottom-0 after:left-0 after:content-[""] after:h-1 after:w-0 after:bg-blue-400
+                after:absolute after:bottom-0 after:right-0 after:content-[""] after:h-1 after:w-0 after:bg-blue-400
                 after:transition-[width] after:duration-500 after:ease-out
                 ${selected ?
-                    'font-semibold bg-blue-400/20 ring ring-blue-200 after:w-full' :
+                    'after:left-0 font-semibold bg-blue-400/20 ring ring-blue-200 after:w-full' :
                     'bg-white hover:text-white hover:bg-blue-400/80'
                 }
             `}>
