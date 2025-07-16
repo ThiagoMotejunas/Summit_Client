feb20f1f6f232816f809608c761b77f00d9c73a6
VitongoviskDev <v_campos_dev@outlook.com>
2025-07-16 14:42:53 -0300

    box shadow in mobile navbar

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -8,7 +8,7 @@ import MenuButtons from '../util/menu';
 const MobileNavbar: React.FC = () => {
 
     return (
-        <div className='fixed bottom-0 w-full h-18 bg-white shadow-[0_-4px_6_-1_black] lg:hidden'>
+        <div className='fixed bottom-0 w-full h-18 bg-white shadow-[0_-4px_6px_-1px_rgb(0_0_0_/_0.1),_0_-2px_4px_-2px_rgb(0_0_0_/_0.1)] lg:hidden'>
             <nav className='flex justify-around items-center h-full px-[4vw] md:justify-center md:gap-12'>
                 {
                     MenuButtons.map((button, index) => (
