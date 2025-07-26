dc028cf6c0b272e41d0d0ea12c7523ef9507503c
VitorCamposDev <v_campos_dev@outlook.com>
2025-07-25 21:32:20 -0300

    fix: carriers responsive grid

--- src/pages/CarriersPage.tsx
+++ src/pages/CarriersPage.tsx
@@ -6,17 +6,19 @@ import DefaultPage from '../components/DefaultPage'
 const CarriersPage: React.FC = () => {
   return (
     <DefaultPage >
-      <section>
-        <h1 className='text-3xl text-center text-blue-500/80 font-medium tracking-[8px]' style={{ fontFamily: 'Oswald, sans-serif' }}>EXPLORE OS CURSOS MAIS PROCURADOS</h1>
-        <p className='text-lg text-center text-gray-600 mt-2'>Descubra formações qualificadas em diferentes campos proffisionais.</p>
-      </section>
-      <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-8'>
-        {
-          Carriers.map((carrier, index) => (
-            <CarrierCard key={index} carrier={carrier} />
-          ))
-        }
-      </section>
+      <div className='ex flex-col justify-center items-center max-w-lg mx-auto h-full md:max-w-3xl'>
+        <section>
+          <h1 className='text-3xl text-center text-blue-500/80 font-medium tracking-[8px]' style={{ fontFamily: 'Oswald, sans-serif' }}>EXPLORE OS CURSOS MAIS PROCURADOS</h1>
+          <p className='text-lg text-center text-gray-600 mt-2'>Descubra formações qualificadas em diferentes campos proffisionais.</p>
+        </section>
+        <section className='w-full grid grid-cols-[repeat(auto-fit,minmax(19rem,1fr))] place-items-center gap-4 mt-8'>
+          {
+            Carriers.map((carrier, index) => (
+              <CarrierCard key={index} carrier={carrier} />
+            ))
+          }
+        </section>
+      </div>
     </DefaultPage>
   )
 }
@@ -29,7 +31,7 @@ const Carriers: CarrierCardDto[] = [
     name: 'Tecnologia',
     description: 'Programação, desenvolvimento web, mobile, inteligência artificial e muito mais.',
     coursesAmount: 141
-  },{
+  }, {
     id: 123,
     name: 'Marketing',
     description: 'Marketing digital, publicidade, branding, redes sociais e estratégias de vendas.',
